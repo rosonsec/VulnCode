8a661eb0eb53648ac2684600d7c53bafc8d18c1d
rosonsec <36442257+rosonsec@users.noreply.github.com>
2019-03-12 20:59:02 +0100

    Buffer OverFlow

---
+++ bof.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	char buff[100];
+	if(argc <2) {
+   		printf("Syntax: %s <input string>\n", argv[0]);
+		exit (0);
+ 	}
+  strcpy(buff, argv[1]);
+  return 0;
+}
